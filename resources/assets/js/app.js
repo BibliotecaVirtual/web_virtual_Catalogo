b2c592d8d27e4ad0692987f6d312ac65b55b99fd
jinmi16 <jinmi_16@hotmail.com>
2016-03-01 17:20:33 -0500

    INICIO DE PROYECTO

---
+++ resources/assets/js/app.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+    // Basic Functions
+    //––––––––––––––––––
+    function windowWidth() {
+        var winWidth = $(window).width();
+        // console.log(winWidth);
+        return winWidth;
+    };
+    function windowHeight() {
+        var winHeight = $(window).height();
+        // console.log(winHeight);
+        return winHeight;
+    };
+
+    // Slider Init
+    //––––––––––––––––––––
+    $('body').css('height', windowHeight());
+
+    // var slider = new Slider();
+    $('#main-slider').Slider({
+        speed: 800,
+        mode: 'horizontal',
+    });
+	
+	hiloSlider = setInterval(function(){ $(".arrow-right").click() }, 5000);
+
+    // Functions onResize
+    //––––––––––––––––––––
+    $(window).resize(function() {
+        $('body').css('height', windowHeight());
+    });
+})
