0a2940e12d3dad30b2ff5076e3b35c0c4d19ab3b
yisraels <yisraels@outlook.com>
2016-04-29 11:57:16 -0500

    mas cambios

--- m/resources/js/device.js
+++ m/resources/js/device.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 
 if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
  {
-window.location = "http://www.google.com/";
+window.location = "http://www.beyeshua.com/bvirtual/m";
 
 }
 else
 {
-window.location = "http://bvirtual.bnp.gob.pe/bv";
+window.location = "http://www.beyeshua.com/bvirtual";
 }
 
 });
